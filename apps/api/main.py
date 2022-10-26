16b352bca15a16f888efee193a2a73b6718d5b84
Valters Vieško <76264011+theglobehead@users.noreply.github.com>
2022-10-26 20:19:32 +0300

    feat: added endpoint to get basic user info (#22)

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Form, status, Response
+from fastapi import FastAPI, Form, status, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 
@@ -71,8 +71,8 @@ def get_searched_users(
 
 @app.post("/get_user_study_sets", status_code=status.HTTP_200_OK)
 def get_user_study_sets(
+        request: Request,
         user_uuid: str = Form(...),
-        token_uuid: str = Form(...),
 ):
     """
     Ajax endpoint for getting a users study sets
@@ -80,6 +80,7 @@ def get_user_study_sets(
     :param token_uuid: the token_uuid of the user who requested it
     :return: A list of dictionaries. Check below
     """
+    token_uuid = request.headers.get("Authorization", default="").replace("Bearer ", "")
     study_sets = []
     user_id = ControllerDatabase.get_user_id_by_uuid(user_uuid)
     requester_user_id = ControllerDatabase.get_user_id_by_token_uuid(token_uuid)
@@ -185,6 +186,30 @@ def get_user_friend_requests(
         })
 
     return {"friend_requests": friend_requests}
+
+
+@app.post("/get_user_info", status_code=status.HTTP_200_OK)
+def get_user_friend_requests(
+        user_uuid: str = Form(...),
+):
+    """
+    Used for getting basic info on the user
+    Can also be used to get a users friends, if is_accepted is true
+    :param user_uuid: The uuid of the user
+    :return: A dictionary
+    """
+    user = ControllerDatabase.get_user_by_uuid(user_uuid)
+
+    user_dict = {
+        "user_uuid": user.user_uuid,
+        "user_name": user.user_name,
+        "user_email": user.user_email,
+        "random_id": user.random_id,
+        "created": user.created.strftime("%Y/%m/%m"),
+    }
+
+    return {"user": user_dict}
+
 
 
 # Methods used for posting
