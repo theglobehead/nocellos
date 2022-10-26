d53eb13716f8d60cb43f13537851cfee2dbb2cab
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-10-26 23:50:27 +0300

    fix: made axios interceptor work

--- apps/www/src/contexts/axios.tsx
+++ apps/www/src/contexts/axios.tsx
@@ -1,4 +1,4 @@
-import { Axios } from 'axios';
+import axios, { Axios } from 'axios';
 import { createContext, useMemo } from 'react';
 
 export const AxiosContext = createContext<Axios>(undefined);
@@ -6,23 +6,25 @@ export const AxiosContext = createContext<Axios>(undefined);
 export default function AxiosProvider({
   children,
 }: React.PropsWithChildren<unknown>) {
-  const axios = useMemo(() => {
-    const axios = new Axios();
-
-    axios.interceptors.request.use((config) => {
-      // Read token for anywhere, in this case directly from localStorage
-      const token = localStorage.getItem('token_uuid');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-
-      return config;
-    });
+  const axiosClient = useMemo(() => {
+    axios.interceptors.request.use(
+      (config) => {
+        // Read token for anywhere, in this case directly from localStorage
+        const token = localStorage.getItem('token_uuid');
+        if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (err) => Promise.reject(err)
+    );
 
     return axios;
   }, []);
 
   return (
-    <AxiosContext.Provider value={axios}>{children}</AxiosContext.Provider>
+    <AxiosContext.Provider value={axiosClient}>
+      {children}
+    </AxiosContext.Provider>
   );
 }
